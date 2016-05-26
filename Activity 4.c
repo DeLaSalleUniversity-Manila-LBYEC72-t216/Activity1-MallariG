e3e0759a7654dc2df7bb83dcd2f9ba5598921f7b
gcmal <gabriel_mallari@dlsu.edu.ph>
2016-05-26 17:28:34 +0800

    Create Activity 4.c

---
+++ Activity 4.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main ()
+{
+	
+	float x, answer;
+	printf("Please Enter the value of your x: ");
+	scanf( "%f" ,&x);
+	
+	answer= x/78;
+	printf ("The value of your f(x) = %f\n", answer);
+	system("PAUSE");
+}
