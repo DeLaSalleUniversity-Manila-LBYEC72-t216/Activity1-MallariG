9a2df631725c96b17bc6d0097cb2e6dea16a9e53
gcmal <gabriel_mallari@dlsu.edu.ph>
2016-05-26 17:27:23 +0800

    Create Activity 2.c

---
+++ Activity 2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+ int base, height; 
+ float area;
+
+
+printf("\n Enter the base:" );
+scanf("%d", &base);
+printf("\n Enter the height:" );
+scanf("%d", &height);
+area= 0.5 *(base*height);
+printf("The area: %f\n", area);
+system("PAUSE");
+return 0;
+
+
+system("PAUSE");
+
+
+}
