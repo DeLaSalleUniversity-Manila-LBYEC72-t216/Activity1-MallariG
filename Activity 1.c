7d16f2ba70697a31fbf4622d239f033aab351ef0
gcmal <gabriel_mallari@dlsu.edu.ph>
2016-05-26 17:26:39 +0800

    Create Activity 1.c

---
+++ Activity 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+printf("@@@@@@@@@@@@@@@@@@@@@@@@\n");
+printf(" \n  Gabriel C. Mallari \n");
+printf("  BS  ECE\n");
+printf("  May 26'' 2016\n");
+printf("\n@@@@@@@@@@@@@@@@@@@@@@@@\n");
+
+
+system("PAUSE");
+
+
+}
