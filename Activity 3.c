0a96966bbc3b70aaf9fd046b93eb8172f82ae64d
gcmal <gabriel_mallari@dlsu.edu.ph>
2016-05-26 17:27:59 +0800

    Create Activity 3.c

---
+++ Activity 3.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+ int year, age;
+ printf("\nEnter year: ");
+ scanf("%d", &year);
+age = 2016-year;
+printf ("Age: %d\n\n", age),
+
+system("PAUSE");
+}
